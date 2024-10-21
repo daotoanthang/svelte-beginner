fdad613bb13094021f70d63cdab74d20d6b603ce
daotoanthang <thangdao2022@gmail.com>
2024-10-21 20:55:38 +0700

    add learning component

---
+++ src/routes/learning/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let name = "";
+  let x = 0;
+  const handleIncrement = () => {
+    x++;
+  };
+</script>
+
+<h1>Hello {name}!</h1>
+<h2>{x}</h2>
+{#if x < 5}
+  <p>{x} is less than 5</p>
+{:else if (x = 5)}
+  <p>{x} is equal 5</p>
+{:else if x > 5}
+  <p>{x} is greater than 5</p>
+{/if}
+<button on:click={handleIncrement}>Increment</button>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
---
+++ src/routes/product/[slug]/+page.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+  // Đây là hàm `load` để lấy dữ liệu liên quan đến `slug`
+  // @ts-ignore
+  export async function load({ params }) {
+    const { slug } = params;
+
+    // Giả sử bạn có API để lấy thông tin sản phẩm dựa trên slug
+    console.log(slug);
+  }
+</script>
+
+<script>
+  export let product = {
+    name: "iphone",
+    description: "iphone",
+    price: "100$",
+  };
+</script>
+
+<h1>{product.name}</h1>
+<p>{product.description}</p>
+<p>Price: ${product.price}</p>
